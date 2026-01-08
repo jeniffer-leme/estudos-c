0d254bcf07adcd9aabaf6fc7ce10d5b4f14c4622
Jeniffer Leme <162703146+jeniffer-leme@users.noreply.github.com>
2026-01-07 23:23:00 -0300

    Create loop_cem.c

---
+++ exercicios/funcoes/loop_cem.c
@@ -0,0 +1,16 @@
+//Criar uma função com for que imprime números de 1 a 100.
+
+#include <stdio.h>
+
+void loop(){
+    for(int i = 1; i <= 100; i++){
+        printf("%d\n", i);
+    }
+}
+
+int main(){
+
+    loop();
+
+    return 0;
+}
